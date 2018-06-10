66511891f57b52f387fa255f08e3e322400d2eb2
Vibs <vibs2006@gmail.com>
2018-06-10 09:44:57 +0530

    ClaimsIdentity Handson

--- Dominik/Program.cs
+++ Dominik/Program.cs
@@ -6,90 +6,57 @@ using System.Threading.Tasks;
 using System.Security.Principal;
 using System.Threading;
 using System.Security.Permissions;
+using System.Security.Claims;
 
 namespace Dominik
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            //var identity = WindowsIdentity.GetCurrent();
-            //Console.WriteLine(identity.User); //SIDs
-            //Console.WriteLine(identity.Name); //UserName with PC Domain
+            var claims = new List<Claim>
+           {
+               new Claim(ClaimTypes.Name, "Vaibhav"),
+               new Claim(ClaimTypes.Email, "cutevibhor@gmail.com"),
+               new Claim(ClaimTypes.Role, "Admin")
+           };
 
-            //var principle = new WindowsPrincipal(identity);
-            //Console.WriteLine("Is in Users Role " + principle.IsInRole("Builtin\\Users"));
+            ClaimsIdentity identity = new ClaimsIdentity(claims,"any identity", ClaimTypes.Name, ClaimTypes.Role);
+            var priciple = new ClaimsPrincipal(identity);
 
-            //var account = new NTAccount(identity.Name);
-            //var sid = account.Translate(typeof(SecurityIdentifier));
-            //write(sid.Value); //Another way to fetch SID
+            corporateIdentity identityCorp = new corporateIdentity("vaibhav", "vaibhavreports", "office");
+            Console.WriteLine("Corp - " + identity.Name);
 
-            //write("Groups (SIDs) - (Their Name Transaction Account)");
-            //foreach (var group in identity.Groups)
-            //{
-            //    write($"({group.Value}) - ({group.Translate(typeof(NTAccount))})");
-            //}
+            //ClaimsPrincipal.Current is also used 
 
-            ////You need to run this in elevated command prompt otherwise it'll return false.
-            //var localAdmins = new SecurityIdentifier(WellKnownSidType.BuiltinAdministratorsSid, null);
-            //write($"Is in Built in Local Administrator role? - {principle.IsInRole(localAdmins)}");
 
-            //var localAdmins1 = new SecurityIdentifier(WellKnownSidType.AccountAdministratorSid, identity.User.AccountDomainSid);
-            //write($"Is in Account Administrator role? - {principle.IsInRole(localAdmins1)}");
+            Thread.CurrentPrincipal = priciple;
 
-            SetupPrincipal();
-            UsePrincipal();
+            Console.WriteLine(identity.IsAuthenticated);
+
+            Console.WriteLine(Thread.CurrentPrincipal.Identity.Name);
+
+            var claimprinciple1 =  (ClaimsPrincipal)(Thread.CurrentPrincipal);
+            foreach (var item in claimprinciple1.Claims)
+                Console.WriteLine(item.Type + " - " + item.Value);
+
+            //Fetch record from identity
+            Console.WriteLine(claimprinciple1.FindFirst(ClaimTypes.Email).Value);
 
 
             Console.ReadLine();
         }
+    }
 
-        private static void write(string s)
+    class corporateIdentity: ClaimsIdentity
+    {
+        public corporateIdentity(string name, string reportsTo, string office)
         {
-            Console.WriteLine(s);
+            AddClaim(new Claim(ClaimTypes.Name, name));
+            AddClaim(new Claim("reportsTo", reportsTo));
         }
+      
 
-        private static void SetupPrincipal()
-        {
-            var identity = new GenericIdentity("bob");
-
-            string[] roles = new string[] { "admin" };
-
-            var genericprinciple = new GenericPrincipal(identity, roles);
-
-            Thread.CurrentPrincipal = genericprinciple;
-
-        }
-        //Error must be handled
-        [PrincipalPermission(SecurityAction.Demand, Role = "Admin")]
-        [PrincipalPermission(SecurityAction.Demand, Role = "Developer")] //You can use multiple roles like this
-        private static void AdminSpecificTask()
-        {
-            write("Admin Specific Task performed");
-        }
-
-        private static void UsePrincipal()
-        {
-            var principle = Thread.CurrentPrincipal;
-
-            write(principle.Identity.Name);
-            write(principle.Identity.IsAuthenticated.ToString());
-            write(principle.IsInRole("admin").ToString());
-
-            //Three approaches for a deciding ona  role type. 
-
-            //Approach 1
-            if (principle.IsInRole("admin"))
-            {
-                write("Role is Admin");
-            }
-
-            //If this fails then it'll raise exception and you need to catch that
-            new PrincipalPermission(null, "Admin").Demand();
-            write("Role passed principle permission so must be fine");
-
-            //Third Aproach
-            AdminSpecificTask();
-        }
+        public string Job { get; set; }
     }
 }
